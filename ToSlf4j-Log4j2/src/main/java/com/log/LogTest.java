0be7e43dd3e828a939f178bd7b21aecf8601abf0
张庆荣 <1006810878@qq.com>
2018-02-25 17:05:27 +0800

    提交代码

--- ToSlf4j-Log4j2/src/main/java/com/log/LogTest.java
+++ ToSlf4j-Log4j2/src/main/java/com/log/LogTest.java
@@ -6,6 +6,9 @@ import com.log.log4j.Log4jTest;
 import com.log.log4j2.Log4j2Test;
 import com.log.logback.LogBackTest;
 import com.log.slf4j.Slf4jTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LogTest {
 
@@ -22,5 +25,18 @@ public class LogTest {
         JdkLogTest.test();
         //commons log
         CommonsLogTest.test();
+    }
+
+    @Test
+    public void testLog() {
+        Logger log = LoggerFactory.getLogger(Slf4jTest.class);
+        for (int i = 0; i < 100000; i++) {
+            log.info("测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志测试日志");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
